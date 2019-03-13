293f2edb25c7a6152c15fff6b1ab68f44cb2c816
Hardik Shah <hrshah5@asu.edu>
2019-03-13 14:17:13 -0700

    First Test Commit

--- cse546Project1/src/com/conn/Home.java
+++ cse546Project1/src/com/conn/Home.java
@@ -27,7 +27,7 @@ public class Home extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("called");
+		System.out.println(" get method called");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
